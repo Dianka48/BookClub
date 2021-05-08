08201c97862f63598f53b06629419664c28dc818
Diana Nemcekova <dianka.melegova@gmail.com>
2021-05-08 07:36:17 +0200

    Initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import styles from './Main.module.css';
+
+const Main = () => {
+  return (
+    <div className={styles.container}>
+      {/* <img src={main} alt="open book on a table" /> */}
+    </div>
+  );
+};
+
+export default Main;
